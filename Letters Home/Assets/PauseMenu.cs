fe8dfc19719b387d3b85519dacb10611446c785c
Kindra Lingenfelter <scrunnchy@gmail.com>
2020-02-15 17:45:17 -0700

    Pause Menu fully implemented

---
+++ Letters Home/Assets/PauseMenu.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool isPaused = false;
+    public GameObject pauseUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    private void Pause()
+    {
+        pauseUI.SetActive(true);
+        Time.timeScale = 0.1f;
+        isPaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+}
