eab73e45307d4fd168a4ab92f9c1460c70541c09
Kindra Lingenfelter <scrunnchy@gmail.com>
2020-02-26 20:33:04 -0700

    fixed position return

--- Letters Home/Assets/PauseMenu.cs
+++ Letters Home/Assets/PauseMenu.cs
@@ -11,6 +11,7 @@ public class PauseMenu : MonoBehaviour
     public GameObject pauseUI;
     private static Vector3 playerPosition;
     private static PlayerMovement pm;
+    private static bool firstLoad = true;
 
     private void Awake()
     {
@@ -21,15 +22,19 @@ public class PauseMenu : MonoBehaviour
     void Start()
     {
         pauseUI.SetActive(false);
-        Resume();
+        if (firstLoad)
+            Time.timeScale = 1f;
+        else
+            Resume();
+        firstLoad = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if(isPaused)
+            if (isPaused)
             {
                 Resume();
             }
@@ -42,17 +47,17 @@ public class PauseMenu : MonoBehaviour
 
     private void Pause()
     {
-        pauseUI.SetActive(true);
         Time.timeScale = 0.1f;
         isPaused = true;
-        playerPosition = pm.transform.localPosition;
+        playerPosition = pm.gameObject.transform.position;
+        pauseUI.SetActive(true);
     }
 
     public void Resume()
     {
-        pauseUI.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
-        pm.transform.localPosition = playerPosition; 
+        pm.transform.position = playerPosition;
+        pauseUI.SetActive(false);
     }
 }
